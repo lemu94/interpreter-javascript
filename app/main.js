7d59ba8cf08471e67e230f8801d4326520526902
YOKOI KPAMISSA LEMUEL-ANGE <lyokoi@reference.ca>
2025-04-12 18:34:33 -0400

    fix(main): add handling for unterminated strings by setting invalid token flag

--- app/main.js
+++ app/main.js
@@ -112,6 +112,7 @@ if (fileContent.length !== 0) {
         
         if(presence) {
           if (lastindexChar === -1){
+            hasInvalidToken = true;
             console.error(`[line ${index + 1}] Error: Unterminated string`);
          }
          else {
